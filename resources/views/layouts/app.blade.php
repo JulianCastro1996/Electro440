42612990bc5c0cbbb4cc12b05e988d5eddf8ab5b
Julian Castro <julian1996castro@gmail.com>
2022-08-09 20:18:30 -0230

    version con estilos sin logo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,9 +22,9 @@
 </head>
 <body>
     <div id="app" class="container">
-        <nav class="navbar navbar-expand-lg sticky-top bg-light">
+        <nav class="navbar navbar-expand-lg sticky-top ">
             <div class="container-fluid">
-                <a class="navbar-brand" href="#">Electro440</a>
+                <a class="navbar-brand" href="/">Electro440</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
                 </button>
--- resources/views/planillas/planilla.blade.php
+++ resources/views/planillas/planilla.blade.php
@@ -3,53 +3,59 @@
 @section('title',"Solicitud Nº: $p->id ")
 
 @section('content')
-<div class="row justify-content-center my-5">
-    <div class="card col-md-6 ">
-        <div class="card-header text-center text-bg-dark ">
-            <h2>Planilla Nº: {{$p->id}}</h2>
-        </div>
-        <ul class="list-group list-group-flush">
-            <li class="list-group-item text-bg-info d-flex justify-content-between align-items-center">
-                <span><strong>Cliente:</strong> {{$p->cliente}}</span>
-            </li>
-            <li class="list-group-item text-bg-info d-flex justify-content-between align-items-center">
-                <span><strong>Contacto:</strong> {{$p->contacto}}</span>
-            </li>
-            <li class="list-group-item text-bg-info d-flex justify-content-between align-items-center">
-                <span><strong>Articulo:</strong> {{$p->articulo}}</span>
-            </li>
-            <li class="list-group-item text-bg-info d-flex justify-content-between align-items-center">
-                <span><strong>Detalle:</strong>  {{$p->detalle}}</span>
-            </li>
-        @if ($p->presupuesto != null)
-            <li class="list-group-item text-bg-warning d-flex justify-content-between align-items-center">
-                <span><strong>Presupuesto:</strong>  {{$p->presupuesto}}.</span>
-            </li>
-            <li class="list-group-item text-bg-warning d-flex justify-content-between align-items-center">
-                <span><strong>Diagnostico:</strong> {{$p->diagnostico}}.</span>
-            </li>
-        </ul>                    
-        @else
-        </ul>
-            <form  action="{{route('presupPlanilla')}}" method="POST" class="row  justify-content-center ">
-            @csrf
-            <div class="row justify-content-center">
-                <input type="hidden" name="id" value="{{$p->id}}">
-                <div class="col-12 my-2">
-                    <input type="number" class="form-control" name="presupuesto" placeholder="Presupuesto" aria-label="Presupuesto">
-                    @error('presupuesto')
-                        <strong class="text-danger">Campo obligatorio</strong>
-                    @enderror
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header text-center text-bg-dark ">
+                    <h2>Planilla Nº: {{$p->id}}</h2>
                 </div>
-                <div class="col-12 my-2">
-                    <textarea class="form-control" name="diagnostico" placeholder="Diagnostico" ></textarea>
+                <div class="card-body">
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item ">
+                            <span><strong>Cliente:</strong> {{$p->cliente}}</span>
+                        </li>
+                        <li class="list-group-item ">
+                            <span><strong>Contacto:</strong> {{$p->contacto}}</span>
+                        </li>
+                        <li class="list-group-item ">
+                            <span><strong>Articulo:</strong> {{$p->articulo}}</span>
+                        </li>
+                        <li class="list-group-item ">
+                            <span><strong>Detalle:</strong>  {{$p->detalle}}</span>
+                        </li>
+                    @if ($p->presupuesto != null)
+                        <li class="list-group-item ">
+                            <span><strong>Presupuesto:</strong>  {{$p->presupuesto}}.</span>
+                        </li>
+                        <li class="list-group-item ">
+                            <span><strong>Diagnostico:</strong> {{$p->diagnostico}}.</span>
+                        </li>
+                    </ul>                    
+                    @else
+                    </ul>
+                        <form  action="{{route('presupPlanilla')}}" method="POST" class="row  justify-content-center ">
+                        @csrf
+                        <div class="row justify-content-center">
+                            <input type="hidden" name="id" value="{{$p->id}}">
+                            <div class="col-12 my-2">
+                                <input type="number" class="form-control" name="presupuesto" placeholder="Presupuesto" aria-label="Presupuesto">
+                                @error('presupuesto')
+                                    <strong class="text-danger">Campo obligatorio</strong>
+                                @enderror
+                            </div>
+                            <div class="col-12 my-2">
+                                <textarea class="form-control" name="diagnostico" placeholder="Diagnostico" ></textarea>
+                            </div>
+                            <input type="submit" class="btn btn-primary my-3 col-3 " value="Presupuestar">
+                        </div>
+                        </form>
+                    @endif                
                 </div>
-                <input type="submit" class="btn btn-primary my-3 col-3 " value="Presupuestar">
+                <div class="card-footer  text-bg-dark">
+                        Recibido: {{$p->created_at}}
+                </div>
             </div>
-            </form>
-        @endif                
-        <div class="card-footer  text-bg-dark">
-            Recibido: {{$p->created_at}}
         </div>
     </div>
 </div>
